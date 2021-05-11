70c2fbb7c2093bab672458b22818112d750ab51f
Antonio Coutinho <antoniorodrigues.coutinho@gmail.com>
2021-05-11 09:44:20 +0100

    Adjust windows design

--- MysqlStudentCRUD/FormStudent.Designer.cs
+++ MysqlStudentCRUD/FormStudent.Designer.cs
@@ -32,7 +32,17 @@ namespace MysqlStudentCRUD
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtReg = new System.Windows.Forms.TextBox();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.txtSection = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -61,10 +71,92 @@ namespace MysqlStudentCRUD
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.txtSection);
+            this.panel2.Controls.Add(this.txtClass);
+            this.panel2.Controls.Add(this.txtReg);
+            this.panel2.Controls.Add(this.txtName);
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
             this.panel2.Location = new System.Drawing.Point(12, 123);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(438, 418);
             this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Class:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Reg.:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Section:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(35, 363);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(35, 79);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(311, 23);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtReg
+            // 
+            this.txtReg.Location = new System.Drawing.Point(35, 157);
+            this.txtReg.Name = "txtReg";
+            this.txtReg.Size = new System.Drawing.Size(311, 23);
+            this.txtReg.TabIndex = 6;
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(35, 227);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(311, 23);
+            this.txtClass.TabIndex = 7;
+            // 
+            // txtSection
+            // 
+            this.txtSection.Location = new System.Drawing.Point(35, 297);
+            this.txtSection.Name = "txtSection";
+            this.txtSection.Size = new System.Drawing.Size(311, 23);
+            this.txtSection.TabIndex = 8;
             // 
             // FormStudent
             // 
@@ -79,6 +171,8 @@ namespace MysqlStudentCRUD
             this.Text = "Student";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -88,5 +182,14 @@ namespace MysqlStudentCRUD
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtSection;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.TextBox txtReg;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
